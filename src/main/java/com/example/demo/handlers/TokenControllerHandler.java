3fe85b9e2a2661b6f2b566c133a0e00fd89a9c7d
izzet_win <izzettin.karasayar@hotmail.com>
2024-12-03 19:38:19 +0300

    feat: add TokenException and TokenControllerHandler classes
    - Created TokenException class to handle token-related exceptions.
    - Created TokenControllerHandler class to handle TokenException and return a structured error response.

---
+++ src/main/java/com/example/demo/handlers/TokenControllerHandler.java
@@ -0,0 +1,27 @@
+package com.example.demo.handlers;
+
+import com.example.demo.exception.TokenException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class TokenControllerHandler {
+    @ExceptionHandler(value = TokenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<ErrorResponse> handleRefreshTokenException(TokenException ex, WebRequest request){
+        final ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .error("Invalid Token")
+                .status(HttpStatus.FORBIDDEN.value())
+                .message(ex.getMessage())
+                .path(request.getDescription(false))
+                .build();
+        return new ResponseEntity<>(errorResponse,HttpStatus.FORBIDDEN);
+    }
+}
