35c9e46db9938c8e92dfb61135a677d76a39d61d
Izzettin Karasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-12-03 13:55:34 +0300

    chore: chore: update import lines in MailRepository

--- src/main/java/com/example/demo/repo/MailRepository.java
+++ src/main/java/com/example/demo/repo/MailRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repo;
 
-import com.example.demo.Entity.Mail;
+import com.example.demo.entity.Mail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface MailRepository extends JpaRepository<Mail,Long> {
