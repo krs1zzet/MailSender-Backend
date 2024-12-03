c8c6af1b19884c66c32d5c00cc4a44389be2272b
Izzettin Karasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-12-03 13:47:55 +0300

    feat : create new directory appConfig

---
+++ src/main/java/com/example/demo/config/appConfig.java
@@ -0,0 +1,10 @@
+package com.example.demo.config;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class appConfig {
+}
