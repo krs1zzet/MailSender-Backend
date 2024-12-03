223339be091b71979471f5a93b1165e75b950f98
Izzettin Karasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-12-03 13:47:47 +0300

    feat : create new directory AuthenticationRequest

---
+++ src/main/java/com/example/demo/payload/request/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
