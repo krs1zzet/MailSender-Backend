de6bd97d0907e170416e0fe30e67aa0ac6affb45
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-12-23 02:32:31 +0300

    feat : adding MailServiceImpl to handle Mail business logic
    - sendMail requires senderId,receiversId and mailTemplateId to send mail with javaMailSender
    - method set SimpleMailMessages with the given instructions and sends the mails in a for loop

--- src/main/java/com/example/demo/features/mailSystem/service/impl/MailServiceImpl.java
+++ src/main/java/com/example/demo/features/mailSystem/service/impl/MailServiceImpl.java
@@ -1,59 +1,57 @@
 package com.example.demo.features.mailSystem.service.impl;
 
-import com.example.demo.features.mailSystem.entity.Mail;
-import com.example.demo.features.mailSystem.dto.MailDTO;
-import com.example.demo.features.mailSystem.dto.converter.MailDtoConverter;
-import com.example.demo.features.mailSystem.dto.request.CreateMailRequest;
-import com.example.demo.features.mailSystem.repository.MailRepository;
+import com.example.demo.features.mailSystem.dto.MailTemplateDTO;
+import com.example.demo.features.mailSystem.dto.ReceiverDTO;
+import com.example.demo.features.mailSystem.dto.SendererDTO;
+import com.example.demo.features.mailSystem.dto.converter.MailTemplateDtoConverter;
+import com.example.demo.features.mailSystem.dto.converter.ReceiverDtoConverter;
+import com.example.demo.features.mailSystem.dto.converter.SendererDtoConverter;
+import com.example.demo.features.mailSystem.entity.MailTemplate;
+import com.example.demo.features.mailSystem.entity.Receiver;
+import com.example.demo.features.mailSystem.entity.Senderer;
 import com.example.demo.features.mailSystem.service.MailService;
-import jakarta.transaction.Transactional;
+import com.example.demo.features.mailSystem.service.MailTemplateService;
+import com.example.demo.features.mailSystem.service.ReceiverService;
+import com.example.demo.features.mailSystem.service.SendererService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class MailServiceImpl implements MailService {
-    private final MailRepository mailRepository;
-    private final MailDtoConverter mailDtoConverter;
 
+    private final JavaMailSender javaMailSender;
+    private final ReceiverService receiverService;
+    private final SendererService sendererService;
+    private final MailTemplateService mailTemplateService;
 
-    public MailServiceImpl(MailRepository mailRepository, MailDtoConverter mailDtoConverter) {
-        this.mailRepository = mailRepository;
-        this.mailDtoConverter = mailDtoConverter;
+    public MailServiceImpl(ReceiverService receiverService, SendererService sendererService, MailTemplateService mailTemplateService, SendererDtoConverter sendererDtoConverter, ReceiverDtoConverter receiverDtoConverter, MailTemplateDtoConverter mailTemplateDtoConverter, JavaMailSender javaMailSender) {
+        this.receiverService = receiverService;
+        this.sendererService = sendererService;
+        this.mailTemplateService = mailTemplateService;
+        this.javaMailSender = javaMailSender;
     }
 
     @Override
-    public List<MailDTO> findAll() {
-        return mailDtoConverter.convert(mailRepository.findAll());
-    }
+    public void sendMail(Long senderID, List<Long> receiverIDs, Long mailTemplateID) {
+        SendererDTO sendererDTO = sendererService.findById(senderID);
+        Senderer senderer = new Senderer(sendererDTO.getId(), sendererDTO.getFname(), sendererDTO.getLname(), sendererDTO.getEmail());
 
-    @Override
-    public MailDTO findByID(Long theID) {
-        Optional<Mail> mail = mailRepository.findById(theID);
-        Mail theMail = mail.orElseThrow(() -> new RuntimeException("Did not find mail id - " + theID));
-        return mailDtoConverter.convert(theMail);
-    }
+        List<ReceiverDTO> receiverDTOS = receiverService.findAll();
+        List<Receiver> receiverList = receiverDTOS.stream().map(receiverDTO -> new Receiver(receiverDTO.getId(), receiverDTO.getFname(), receiverDTO.getLname(), receiverDTO.getEmail(), receiverDTO.getGroupName())).toList();
 
-    @Transactional
-    @Override
-    public void save(CreateMailRequest request) {
-        Mail theMail = new Mail();
-        theMail.setBody(request.getBody());
-        theMail.setHeader(request.getHeader());
-        mailRepository.save(theMail);
-    }
+        MailTemplateDTO mailTemplateDTO = mailTemplateService.findByID(mailTemplateID);
+        MailTemplate mailTemplate = new MailTemplate(mailTemplateDTO.getId(), mailTemplateDTO.getHeader(), mailTemplateDTO.getBody());
 
-    @Transactional
-    @Override
-    public void deleteByID(Long theID) {
-        Optional<Mail> mail = mailRepository.findById(theID);
-        Mail theMail = mail.orElseThrow(() -> new RuntimeException("did not find the mail - " + theID));
-        deleteByID(theMail.getId());
+        for (Receiver receiver : receiverList) {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderer.getEmail());
+            mailMessage.setTo(receiver.getEmail());
+            mailMessage.setSubject(mailTemplate.getHeader());
+            mailMessage.setText(mailTemplate.getBody());
+            javaMailSender.send(mailMessage);
+        }
     }
-
-//    @Override
-//    public void assignSendererByMail(Long mailID, String sendererMail) {
-//
-//    }
 }
