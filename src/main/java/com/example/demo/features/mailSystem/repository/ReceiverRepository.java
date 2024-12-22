bcac67e23386aca09a56c0bfc52bb62cafd9a0e5
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-12-23 02:27:42 +0300

    chor : delete mail_ids field in ReceiverDtoConverter

--- src/main/java/com/example/demo/features/mailSystem/repository/ReceiverRepository.java
+++ src/main/java/com/example/demo/features/mailSystem/repository/ReceiverRepository.java
@@ -4,7 +4,9 @@ import com.example.demo.features.mailSystem.entity.Receiver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReceiverRepository extends JpaRepository<Receiver,Long> {
-    public void deleteByStudentAge(Long id);
+
 }
