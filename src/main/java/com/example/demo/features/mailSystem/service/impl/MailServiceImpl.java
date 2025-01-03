f924ae0c3cfe34c827347131873483a756de53b6
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-01-03 10:44:12 +0300

    chor : organize the constructors of mailTemplate,receiver and sender because of the Added Field Event

--- src/main/java/com/example/demo/features/mailSystem/service/impl/MailServiceImpl.java
+++ src/main/java/com/example/demo/features/mailSystem/service/impl/MailServiceImpl.java
@@ -9,10 +9,7 @@ import com.example.demo.features.mailSystem.dto.converter.SenderDtoConverter;
 import com.example.demo.features.mailSystem.entity.MailTemplate;
 import com.example.demo.features.mailSystem.entity.Receiver;
 import com.example.demo.features.mailSystem.entity.Sender;
-import com.example.demo.features.mailSystem.service.MailService;
-import com.example.demo.features.mailSystem.service.MailTemplateService;
-import com.example.demo.features.mailSystem.service.ReceiverService;
-import com.example.demo.features.mailSystem.service.SenderService;
+import com.example.demo.features.mailSystem.service.*;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -26,24 +23,26 @@ public class MailServiceImpl implements MailService {
     private final ReceiverService receiverService;
     private final SenderService senderService;
     private final MailTemplateService mailTemplateService;
+    private final EventService eventService;
 
-    public MailServiceImpl(ReceiverService receiverService, SenderService senderService, MailTemplateService mailTemplateService, SenderDtoConverter senderDtoConverter, ReceiverDtoConverter receiverDtoConverter, MailTemplateDtoConverter mailTemplateDtoConverter, JavaMailSender javaMailSender) {
+    public MailServiceImpl(ReceiverService receiverService, SenderService senderService, MailTemplateService mailTemplateService, SenderDtoConverter senderDtoConverter, ReceiverDtoConverter receiverDtoConverter, MailTemplateDtoConverter mailTemplateDtoConverter, JavaMailSender javaMailSender, EventService eventService) {
         this.receiverService = receiverService;
         this.senderService = senderService;
         this.mailTemplateService = mailTemplateService;
         this.javaMailSender = javaMailSender;
+        this.eventService = eventService;
     }
 
     @Override
     public void sendMail(Long senderID, List<Long> receiverIDs, Long mailTemplateID) {
         SenderDTO senderDTO = senderService.findById(senderID);
-        Sender sender = new Sender(senderDTO.getId(), senderDTO.getFname(), senderDTO.getLname(), senderDTO.getEmail());
+        Sender sender = new Sender(senderDTO.getId(), senderDTO.getFname(), senderDTO.getLname(), senderDTO.getEmail(),eventService.findById_ReturnEvent(senderDTO.getEventId()));
 
         List<ReceiverDTO> receiverDTOS = receiverService.findAll();
-        List<Receiver> receiverList = receiverDTOS.stream().map(receiverDTO -> new Receiver(receiverDTO.getId(), receiverDTO.getFname(), receiverDTO.getLname(), receiverDTO.getEmail(), receiverDTO.getGroupName())).toList();
+        List<Receiver> receiverList = receiverDTOS.stream().map(receiverDTO -> new Receiver(receiverDTO.getId(), receiverDTO.getFname(), receiverDTO.getLname(), receiverDTO.getEmail(), receiverDTO.getGroupName(),eventService.findById_ReturnEvent(receiverDTO.getEventId()))).toList();
 
         MailTemplateDTO mailTemplateDTO = mailTemplateService.findByID(mailTemplateID);
-        MailTemplate mailTemplate = new MailTemplate(mailTemplateDTO.getId(), mailTemplateDTO.getHeader(), mailTemplateDTO.getBody());
+        MailTemplate mailTemplate = new MailTemplate(mailTemplateDTO.getId(), mailTemplateDTO.getHeader(), mailTemplateDTO.getBody(),eventService.findById_ReturnEvent(mailTemplateDTO.getEventId()));
 
         for (Receiver receiver : receiverList) {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
