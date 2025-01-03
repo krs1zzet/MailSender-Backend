a853006d7306cdc4bfffbd33073344524986296c
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-01-03 10:48:32 +0300

    feat: added Custom Query to handle findByEventId operation

--- src/main/java/com/example/demo/features/mailSystem/repository/ReceiverRepository.java
+++ src/main/java/com/example/demo/features/mailSystem/repository/ReceiverRepository.java
@@ -1,12 +1,16 @@
 package com.example.demo.features.mailSystem.repository;
 
+import com.example.demo.features.mailSystem.entity.MailTemplate;
 import com.example.demo.features.mailSystem.entity.Receiver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReceiverRepository extends JpaRepository<Receiver,Long> {
+    @Query("SELECT mt FROM Receiver mt WHERE mt.event.id = :eventId")
+    List<Receiver> findByEventId(Long eventId);
 
 }
--- src/main/java/com/example/demo/features/mailSystem/repository/SenderRepository.java
+++ src/main/java/com/example/demo/features/mailSystem/repository/SenderRepository.java
@@ -2,8 +2,13 @@ package com.example.demo.features.mailSystem.repository;
 
 import com.example.demo.features.mailSystem.entity.Sender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SenderRepository extends JpaRepository<Sender,Long> {
+    @Query("SELECT mt FROM Sender mt WHERE mt.event.id = :eventId")
+    List<Sender> findByEventId(Long eventId);
 }
