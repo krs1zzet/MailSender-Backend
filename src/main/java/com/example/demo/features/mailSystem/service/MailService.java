f30c84687ba75daba6bd888840796bf8bb10c583
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-02-18 23:54:18 +0300

    chor: refactor the Method type to handle custom repsonses to frontend

--- src/main/java/com/example/demo/features/mailSystem/service/MailService.java
+++ src/main/java/com/example/demo/features/mailSystem/service/MailService.java
@@ -1,9 +1,10 @@
 package com.example.demo.features.mailSystem.service;
 
+import com.example.demo.features.mailSystem.dto.MailDTO;
 import com.example.demo.features.mailSystem.entity.Receiver;
 
 import java.util.List;
 
 public interface MailService {
-    void sendMail(Long senderID, List<Long> receiverIDs,Long mailTemplateID);;
+    MailDTO sendMail(Long senderID, List<Long> receiverIDs, Long mailTemplateID);;
 }
