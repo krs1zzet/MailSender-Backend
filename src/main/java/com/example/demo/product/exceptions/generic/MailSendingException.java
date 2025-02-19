3a36caeec2a03facf41be67810e3c5d199650801
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-02-19 14:26:39 +0300

    chor : remove unused exception classes

--- src/main/java/com/example/demo/product/exceptions/generic/MailSendingException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.product.exceptions.generic;
-
-public class MailSendingException extends RuntimeException {
-    public MailSendingException(String message) {
-        super(message);
-    }
-}
