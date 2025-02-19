62446e56a55eb195ee8aefbf784c75df1b882e2b
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-02-19 14:27:56 +0300

    feat : add constant validationMessages to handle validation operations in request clases
    - it improves to get input valid datas

---
+++ src/main/java/com/example/demo/features/mailSystem/constants/validationMessages.java
@@ -0,0 +1,22 @@
+package com.example.demo.features.mailSystem.constants;
+
+public class validationMessages {
+    public static final String SENDER_NULL_PASSWORD = "Sender password cannot be empty";
+    public static final String SENDER_INVALID_EMAIL = "Sender email is invalid";
+    public static final String SENDER_BLANK_EMAIL = "Sender email cannot be blank";
+
+
+
+    public static final String EVENT_NULL_PASSWORD = "Event password cannot be empty";
+    public static final String EVENT_NULL_NAME = "Event name cannot be empty";
+//    public static final String EVENT_NULL_DESCRIPTION = "Event description cannot be empty";
+
+    public static final String MAIL_TEMPLATE_NULL_HEADER = "Mail template header cannot be empty";
+    public static final String MAIL_TEMPLATE_NULL_BODY = "Mail template body cannot be empty";
+
+    public static final String RECEIVER_BLANK_EMAIL = "Receiver email cannot be blank";
+    public static final String RECEIVER_INVALID_EMAIL = "Receiver email is invalid";
+
+    private validationMessages() {} // Prevent instantiation
+
+}
