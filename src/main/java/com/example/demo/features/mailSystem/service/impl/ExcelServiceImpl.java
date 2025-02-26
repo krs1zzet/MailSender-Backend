cb46b49f9b4b67433f9dfe4f2e14b1456ea612af
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-02-26 13:07:40 +0300

    chor : added id section for excel save operations
    - with this param we can assign receivers to specific events

--- src/main/java/com/example/demo/features/mailSystem/service/impl/ExcelServiceImpl.java
+++ src/main/java/com/example/demo/features/mailSystem/service/impl/ExcelServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.features.mailSystem.service.impl;
 import com.example.demo.features.mailSystem.dto.request.CreateReceiverRequest;
 import com.example.demo.features.mailSystem.entity.Receiver;
 import com.example.demo.features.mailSystem.repository.ReceiverRepository;
+import com.example.demo.features.mailSystem.service.EventService;
 import com.example.demo.features.mailSystem.service.ExcelService;
 import com.example.demo.features.mailSystem.service.ReceiverService;
 import jakarta.transaction.Transactional;
@@ -22,29 +23,37 @@ import static org.apache.xmlbeans.impl.store.Public2.save;
 public class ExcelServiceImpl implements ExcelService {
 
     private final ReceiverRepository receiverRepository;
+    private final EventService eventService;
+    private final ReceiverService receiverService;
 
-    public ExcelServiceImpl(ReceiverRepository receiverRepository) {
+    public ExcelServiceImpl(ReceiverRepository receiverRepository, EventService eventService, ReceiverService receiverService) {
         this.receiverRepository = receiverRepository;
+        this.eventService = eventService;
+        this.receiverService = receiverService;
     }
 
     @Transactional
     @Override
-    public void saveReceiversFromExcel(MultipartFile file) throws IOException {
-        List<Receiver> receivers = new ArrayList<>();
+    public void saveReceiversFromExcel(MultipartFile file, Long eventId) throws IOException {
+        List<CreateReceiverRequest> receivers = new ArrayList<>();
         try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
             Sheet sheet = workbook.getSheetAt(0);
             for (Row row : sheet) {
                 if (row.getRowNum() == 0) {
                     continue;
                 }
-                Receiver receiver = new Receiver();
-                receiver.setFname(row.getCell(0).getStringCellValue());
-                receiver.setLname(row.getCell(1).getStringCellValue());
-                receiver.setGroupName(row.getCell(2).getStringCellValue());
-                receiver.setEmail(row.getCell(3).getStringCellValue());
-                receivers.add(receiver);
+                CreateReceiverRequest receiverRequest = new CreateReceiverRequest();
+                receiverRequest.setFname(row.getCell(0).getStringCellValue());
+                receiverRequest.setLname(row.getCell(1).getStringCellValue());
+                receiverRequest.setGroupName(row.getCell(2).getStringCellValue());
+                receiverRequest.setEmail(row.getCell(3).getStringCellValue());
+                receiverRequest.setEventId(eventId);
+                receivers.add(receiverRequest);
             }
         }
-        receiverRepository.saveAll(receivers);
+        for (CreateReceiverRequest receiver : receivers) {
+            receiverService.save(receiver);
+        }
+
     }
 }
