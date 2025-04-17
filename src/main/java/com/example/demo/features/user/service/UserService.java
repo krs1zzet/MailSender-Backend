a7ddc8159dbd39ebad94da4bbd768c4a525f5ed5
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-04-17 15:24:12 +0300

    feat: update findById method to use Long type and add findAuthenticatedUser method

--- src/main/java/com/example/demo/features/user/service/UserService.java
+++ src/main/java/com/example/demo/features/user/service/UserService.java
@@ -4,12 +4,16 @@ package com.example.demo.features.user.service;
 import com.example.demo.features.user.dto.FullNameRequestDTO;
 import com.example.demo.features.user.dto.UsagePurposeRequestDTO;
 import com.example.demo.features.user.entity.UserEntity;
+import com.example.demo.features.user.repo.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
 
   UserEntity findByEmail(String email);
 
-  UserEntity findById(Integer id);
+  UserEntity findById(Long id);
 
   void saveUser(UserEntity user);
 
@@ -18,4 +22,6 @@ public interface UserService {
   boolean existsByEmail(String email);
 
   void updateUserPurpose(UsagePurposeRequestDTO userPurposeRequest);
+
+  UserEntity findAuthenticatedUser();
 }
