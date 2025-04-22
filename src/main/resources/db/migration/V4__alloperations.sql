b4f6cd13fdea7acaf5745e58c572b8d33a7b409e
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-04-22 19:41:55 +0300

    feat: add SQL script to drop unique constraint on receiver table

--- src/main/resources/db/migration/V4__alloperations.sql
+++ src/main/resources/db/migration/V4__alloperations.sql
@@ -1,1 +1,11 @@
-ALTER TABLE receiver DROP CONSTRAINT IF EXISTS uk2o1lmleg9c6ephnle40pt7rn3;
+DO $$
+    BEGIN
+        IF EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_name = 'receiver'
+        ) THEN
+            EXECUTE 'ALTER TABLE receiver DROP CONSTRAINT IF EXISTS uk2o1lmleg9c6ephnle40pt7rn3';
+        END IF;
+    END
+$$;
