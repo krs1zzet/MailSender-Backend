fad3897fabbc726d0314e0eba7dc283975105237
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-04-22 13:34:14 +0300

    feat: add SQL script to drop unique constraint on receiver table

--- src/main/resources/db/migration/V1__removeconstratint.sql
+++ src/main/resources/db/migration/V1__removeconstratint.sql
@@ -1,4 +1,4 @@
-CREATE TABLE test_flyway (
-                             id SERIAL PRIMARY KEY,
-                             name VARCHAR(255)
-);
+-- CREATE TABLE test_flyway (
+--                              id SERIAL PRIMARY KEY,
+--                              name VARCHAR(255)
+-- );
--- src/main/resources/db/migration/V3__dropconstraint.sql
+++ src/main/resources/db/migration/V3__dropconstraint.sql
@@ -1,1 +1,1 @@
-ALTER TABLE receiver DROP CONSTRAINT uk2o1lmleg9c6ephnle40pt7rn3;
+-- ALTER TABLE receiver DROP CONSTRAINT uk2o1lmleg9c6ephnle40pt7rn3;
