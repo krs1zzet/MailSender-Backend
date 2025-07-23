3cd251abd4afe8914e11bb4e3a26dd44cc21fbea
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-07-23 16:05:34 +0300

    Change config files

--- src/main/java/com/example/demo/product/config/CorsConfig.java
+++ src/main/java/com/example/demo/product/config/CorsConfig.java
@@ -4,19 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("localhost:8080","fastidious-queijadas-9bb76b.netlify.app","localhost:3000")
-            .allowedOrigins("http://localhost:3000")// Allowed frontend origin
-
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true);
+            // Bütün izin verilen adresleri tek bir .allowedOrigins() metodu içinde listelemelisiniz.
+            .allowedOrigins(
+                    "http://localhost:3000",
+                    "http://localhost:8080",
+                    "https://fastidious-queijadas-9bb76b.netlify.app",
+                    "https://d42pbemxfsmlh.cloudfront.net" // YENİ EKLENEN ADRES
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
   }
 }
